26f270b86b11b686fe9354b88e4e6adae0163e0e
Colin Tindle <ampersand@underscore.space>
2018-06-21 16:03:44 +0100

    form submit adds to reading list and resets form

--- js/app.js
+++ js/app.js
@@ -2,9 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
   const form = document.querySelector('#new-item-form')
-  form.addEventListener('click', function(e) {
+  form.addEventListener('submit', function(e) {
     e.preventDefault()
     console.log('form submitted')
+
+    const title = e.target.title.value
+    const author = e.target.author.value
+    const category = e.target.category.value
+
+    const readingList = document.querySelector('#reading-list')
+
+    const readingListItem = document.createElement('p')
+    const itemTitle = document.createElement('h2')
+    const itemAuthor = document.createElement('p')
+    const itemCategory = document.createElement('p')
+
+    itemTitle.textContent = title
+    itemAuthor.textContent = author
+    itemCategory.textContent = category
+
+    readingListItem.appendChild(itemTitle)
+    readingListItem.appendChild(itemAuthor)
+    readingListItem.appendChild(itemCategory)
+
+    readingList.appendChild(readingListItem)
+
+    e.target.reset()
   })
 
   const deleteAll = document.querySelector('#delete-all')
